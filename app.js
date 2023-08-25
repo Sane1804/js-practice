92d6fdcfa6e800e32dbe98262602c513cf75ebfb
Alexander Vielma <vielmaa1804@gmail.com>
2023-08-25 14:12:49 -0400

    Create user choice

--- app.js
+++ app.js
@@ -1,7 +1,22 @@
-const getComputerChoice = () => {
-    let arr = ["Rock", "Paper", "Scissors"];
-    let ramdomIndex = Math.floor(Math.random() * arr.length);
-    return arr[ramdomIndex];
+
+
+console.log("Welcome, This is rock, paper and scissors game. Hope you enjoy")
+
+
+const userChoice = () => {
+    let input = prompt("enter your choice 'rock' 'paper' 'scissors'").toLocaleLowerCase();
+
+    if (input == "rock" || input == "paper" || input == "scissors"){
+        return input
+    } else {
+        return "Not a valid option. Try again"
+    }
 }
 
-console.log(getComputerChoice());
+
+const getComputerChoice = () => {
+    let arr = ["rock", "paper", "scissors"];
+    let randomIndex = Math.floor(Math.random() * arr.length);
+    return arr[randomIndex];
+}
+
