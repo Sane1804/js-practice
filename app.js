9aaaca95f8618f8103fd57a702a3806da7d71796
Alexander Vielma <vielmaa1804@gmail.com>
2023-09-27 15:43:17 -0400

    Add styles and target hands containers with DOM

--- app.js
+++ app.js
@@ -1,2 +1,21 @@
+const hands = document.querySelectorAll(".real > div");
+
+const cardHeader = document.querySelector(".user-choice");
+
+
+
+hands.forEach(hand => hand.addEventListener("click", () => {
+    const choice = hand.className;
+    if (cardHeader.textContent == ""){
+        cardHeader.textContent = choice;
+    } 
+
+}))
+
+console.log(cardHeader.textContent);
+
+
+
+
 
 
