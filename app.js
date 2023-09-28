fb2b5d6a0a3ca3c0bd7d65542efb5ba57a2de690
Alexander Vielma <vielmaa1804@gmail.com>
2023-09-28 10:18:57 -0400

    Add funtionality to the selected tag

--- app.js
+++ app.js
@@ -1,21 +1,22 @@
-const hands = document.querySelectorAll(".real > div");
+const hands = document.querySelectorAll(".real > div > img");
 
 const cardHeader = document.querySelector(".user-choice");
 
+const displayUserChoice = (clicked) => {
+    hands.forEach(element => {
+        if (element.parentElement.className !== clicked){
+            element.parentElement.style.display = "none"
+            cardHeader.textContent = clicked;
+        }
+    })
+}
 
+const addClassToCliked = (e) => {
+    const CLICKED = e.target.parentElement.className;
+    e.target.style.maxWidth = "350px"
+    displayUserChoice(CLICKED)
+}
 
-hands.forEach(hand => hand.addEventListener("click", () => {
-    const choice = hand.className;
-    if (cardHeader.textContent == ""){
-        cardHeader.textContent = choice;
-    } 
-
-}))
-
-console.log(cardHeader.textContent);
-
-
-
-
+hands.forEach(hand => hand.addEventListener("click", addClassToCliked))
 
 
